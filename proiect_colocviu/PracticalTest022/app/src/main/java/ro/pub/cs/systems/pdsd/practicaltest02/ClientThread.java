d2f00e744552b4a3c97d7d88c94ef5ce2e208595
andreeadanielachivu <andreea.daniela.chivu@gmail.com>
2016-05-17 08:39:35 +0300

    first tasks

---
+++ proiect_colocviu/PracticalTest022/app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/ClientThread.java
@@ -0,0 +1,7 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+/**
+ * Created by Andreea on 5/17/2016.
+ */
+public class ClientThread {
+}
---
+++ proiect_colocviu/PracticalTest022/app/src/main/java/ro/pub/cs/systems/pdsd/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,156 @@
+package ro.pub.cs.systems.pdsd.practicaltest02;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    // Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText wordEditText = null;
+    private Button sendWordButton = null;
+    private TextView wordTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Server port should be filled!",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() != null) {
+                serverThread.start();
+            } else {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not creat server thread!");
+            }
+
+        }
+    }
+
+
+    private GetWeatherForecastButtonClickListener getWeatherForecastButtonClickListener = new GetWeatherForecastButtonClickListener();
+    private class GetWeatherForecastButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String clientAddress = clientAddressEditText.getText().toString();
+            String clientPort    = clientPortEditText.getText().toString();
+            if (clientAddress == null || clientAddress.isEmpty() ||
+                    clientPort == null || clientPort.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Client connection parameters should be filled!",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            if (serverThread == null || !serverThread.isAlive()) {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] There is no server to connect to!");
+                return;
+            }
+
+            String city = cityEditText.getText().toString();
+            String informationType = informationTypeSpinner.getSelectedItem().toString();
+            if (city == null || city.isEmpty() ||
+                    informationType == null || informationType.isEmpty()) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Parameters from client (city / information type) should be filled!",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            weatherForecastTextView.setText(Constants.EMPTY_STRING);
+
+            clientThread = new ClientThread(
+                    clientAddress,
+                    Integer.parseInt(clientPort),
+                    city,
+                    informationType,
+                    weatherForecastTextView);
+            clientThread.start();
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_edit_text);
+        connectButton = (Button)findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientAddressEditText = (EditText)findViewById(R.id.client_address_edit_text);
+        clientPortEditText = (EditText)findViewById(R.id.client_port_edit_text);
+        wordEditText = (EditText)findViewById(R.id.city_edit_text);
+        sendWordButton = (Button)findViewById(R.id.get_weather_forecast_button);
+        sendWordButton.setOnClickListener(getWeatherForecastButtonClickListener);
+        wordTextView = (TextView)findViewById(R.id.word_text_view);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_practical_test02_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
